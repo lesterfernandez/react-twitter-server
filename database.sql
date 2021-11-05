05c19d20e6843caa959b349e69cc8040bfdfcecf
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-11-05 15:19:46 -0400

    done

--- database.sql
+++ database.sql
@@ -1,18 +1,5 @@
 -- Keep track of sql command for setup
 
--- CREATE TABLE users(
---     id SERIAL PRIMARY KEY,
---     fullname VARCHAR(255) NOT NULL
--- );
-
--- ALTER TABLE users ADD COLUMN img VARCHAR(255); 
-
--- used to grab user from google login
--- ALTER TABLE users ADD COLUMN google_id VARCHAR(255); 
-
--- make column unique
--- ALTER TABLE users ADD CONSTRAINT unique_google_id UNIQUE (google_id);
-
 CREATE DATABASE twitter_clone;
 
 CREATE TABLE users (
@@ -27,14 +14,6 @@ CREATE TABLE posts (
     post_author INT REFERENCES users(id) NOT NULL,
     body VARCHAR(255) NOT NULL
 );
-
--- Comments not implemented in order to keep project small
--- CREATE TABLE comments (
---     id SERIAL PRIMARY KEY,
---     post_id INT REFERENCES posts(id) NOT NULL,
---     comment_author INT REFERENCES users(id) NOT NULL,
---     body VARCHAR(255) NOT NULL
--- );
 
 -- new post 
 INSERT INTO posts (post_author, body) VALUES(userid, bodyofpost);
