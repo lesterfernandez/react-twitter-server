13ae7f9133ec5ec7add9bf002bc6e0b9a8ee0bb6
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-11-06 11:03:05 -0400

    done

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(
       // secure: `${inProd ? "true" : "auto"}`, // only https, auto when dev, true when prod
       maxAge: 1000 * 60 * 60 * 24 * 14, // expiration time
       sameSite: "lax",
-      secure: "true",
+      secure: "auto",
     },
   })
 );
