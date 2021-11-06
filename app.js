7cc02a1eb200c009201c5307babdc28779a983f7
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-11-06 11:15:32 -0400

    done

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ const app = express();
 // Allows body to be parsed as json
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_ROOT || "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,HEAD,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "content-Type,x-requested-with");
+  next();
+});
 // Allows cookies to be sent to url on different domain
 app.use(cors({ origin: process.env.CLIENT_ROOT, credentials: true }));
 app.set("trust proxy", 1); // if behind proxy
@@ -22,11 +28,10 @@ app.use(
     resave: true, // resave cookies even if nothing changed
     saveUninitialized: false,
     cookie: {
-      // sameSite: `${inProd ? "none" : "lax"}`, // cross site // set lax while working with http:localhost, but none when in prod
-      // secure: `${inProd ? "true" : "auto"}`, // only https, auto when dev, true when prod
+      sameSite: `${inProd ? "none" : "lax"}`, // cross site // set lax while working with http:localhost, but none when in prod
+      secure: `${inProd ? "true" : "auto"}`, // only https, auto when dev, true when prod
       maxAge: 1000 * 60 * 60 * 24 * 14, // expiration time
-      sameSite: "none",
-      secure: "true",
+      httpOnly: "false",
     },
   })
 );
