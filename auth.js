3f8d696b153d0eca6b4fd6b94e1cfc08be2845c8
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-11-06 10:36:48 -0400

    done

--- auth.js
+++ auth.js
@@ -24,7 +24,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.SERVER_URL + "/auth/google/callback",
     },
     async (_, __, profile, done) => {
       // make account in database
